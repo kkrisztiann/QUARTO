2d77f64c6ae08548fa2509b6b154c2c2a97fc956
Adesz05 <kerekadamtamas@turr.hu>
2022-11-18 11:31:54 +0100

    X gombra megkérdezi hogy biztos vagy-e benne

---
+++ QUARTO_jatek/Players.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUARTO_jatek
+{
+    class Players
+    {
+        public int Id;
+        public string Nev;
+     
+        public Players(int id, string nev)
+        {
+            Id = id;
+            Nev = nev;
+        }
+    }
+}
